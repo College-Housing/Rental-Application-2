438d7f491cfc2462d805d02103c2885f419ba200
Lina <lina.ahmedkamal@gmail.com>
2020-05-06 21:41:04 +0200

    add missing line

--- js/preview.js
+++ js/preview.js
@@ -4,8 +4,7 @@ $('#previewBtn').click(function() {
      $("form input[type='text'], form input[type='email'], form input[type='number'], form input[type='checkbox'], form select, form textarea, form input[type='radio']:checked").each(
         function(index){
           var input = $(this);
-          // && !(input.hasClass('hidden'))
-          if (!(input.val()==="") && !(input.val()===null) ) {
+          if (!(input.val()==="") && !(input.val()===null) && !(input.hasClass('hidden')) ) {
             $("#prevTable").append('<tr class="d-flex"><th class="col-5">'+input.attr('data-table')+'</th><td class="col-7">'+input.val()+'</td></tr>');
           }
           else {
