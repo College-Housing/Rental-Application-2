3528cb27edf3442e19881808535de863862ab0fa
Lina <lina.ahmedkamal@gmail.com>
2020-05-16 00:02:32 +0200

    add validation for email

--- js/email_validation.js
+++ js/email_validation.js
@@ -1,3 +1,40 @@
+
+var filledEmail = [];
+
+document.querySelectorAll("input[type='email']").forEach((mailField,i) => {
+  mailField.addEventListener("change", e => {
+    const email = e.target.value;
+    const hasMail = filledEmail.find(x => x === email);
+    if (!hasMail) {
+      e.target.classList.remove("is-invalid");
+      e.target.classList.remove("email-used-once");
+
+      // $("#"+e.target.id).siblings(".invalid-feedback-2" ).hide();
+      // console.log("This is a unique address");
+      filledEmail = filledEmail.filter((x, j)=> j!==i);
+      filledEmail.push(email);
+      filledEmail = jQuery.grep(filledEmail, function(value) {
+        return value != "";
+      });
+
+    }
+    else {
+      e.target.classList.add("is-invalid");
+      e.target.classList.add("email-used-once");
+
+      // $("#"+e.target.id).siblings(".invalid-feedback-2" ).show();
+      $("#"+e.target.id).siblings(".invalid-feedback" ).html("You already used this email address in the form");
+
+      // e.target.classList.add("is-invalid");
+
+
+      // console.log("this is a dup");
+
+    }
+    // console.log('filled mails without duplicates', filledEmail)
+  });
+});
+
 $("#applicant-email").change(function() {
 
   var restURL= "http://apilayer.net/api/check?access_key=9602c92a17b0e0b7dcf8436c3a53e836&email="+$("#applicant-email").val()+"&smtp=1&format=1";
@@ -15,15 +52,20 @@ $("#applicant-email").change(function() {
 
 function renderList(data) {
   if ((data.format_valid == true) && (data.smtp_check == true) ) {
-    console.log("Email address is valid");
-    $("#applicant-email").removeClass("is-invalid");
-    $("#applicant-email").addClass("is-valid");
+    // console.log("Email address is valid");
+    if (!($("#applicant-email").hasClass("email-used-once"))) {
+
+      $("#applicant-email").removeClass("is-invalid");
+    }
+    // $("#applicant-email").addClass("is-valid");
     return true;
   }
   else {
-    console.log("Email address is not valid");
-    $("#applicant-email").removeClass("is-valid");
+    // console.log("Email address is not valid");
+    // $("#applicant-email").removeClass("is-valid");
     $("#applicant-email").addClass("is-invalid");
+    $("#applicant-email").siblings(".invalid-feedback" ).html("Please enter a real email address");
+
 
     return false;
   }
@@ -51,15 +93,20 @@ $("#parent-1-email").change(function() {
 
 function renderList2(data) {
   if ((data.format_valid == true) && (data.smtp_check == true) ) {
-    console.log("Email address is valid");
-    $("#parent-1-email").removeClass("is-invalid");
-    $("#parent-1-email").addClass("is-valid");
+    // console.log("Email address is valid");
+    if (!($("#parent-1-email").hasClass("email-used-once"))) {
+
+      $("#parent-1-email").removeClass("is-invalid");
+    }
+    // $("#parent-1-email").addClass("is-valid");
     return true;
   }
   else {
-    console.log("Email address is not valid");
-    $("#parent-1-email").removeClass("is-valid");
+    // console.log("Email address is not valid");
+    // $("#parent-1-email").removeClass("is-valid");
     $("#parent-1-email").addClass("is-invalid");
+    $("#parent-1-email").siblings(".invalid-feedback" ).html("Please enter a real email address");
+
 
     return false;
   }
@@ -81,15 +128,20 @@ $("#parent-2-email").change(function() {
 
 function renderList3(data) {
   if ((data.format_valid == true) && (data.smtp_check == true) ) {
-    console.log("Email address is valid");
-    $("#parent-2-email").removeClass("is-invalid");
-    $("#parent-2-email").addClass("is-valid");
+    if (!($("#parent-2-email").hasClass("email-used-once"))) {
+
+      $("#parent-2-email").removeClass("is-invalid");
+    // console.log("Email address is valid");
+    // $("#parent-2-email").addClass("is-valid");
+    }
     return true;
   }
   else {
-    console.log("Email address is not valid");
-    $("#parent-2-email").removeClass("is-valid");
+    // console.log("Email address is not valid");
+    // $("#parent-2-email").removeClass("is-valid");
     $("#parent-2-email").addClass("is-invalid");
+    $("#parent-2-email").siblings(".invalid-feedback" ).html("Please enter a real email address");
+
 
     return false;
   }
